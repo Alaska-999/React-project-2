73d85e5e9d048bfc26de8cd950e24cf22e27bb04
Alaska-999 <kristinagaranchuk@gmail.com>
2022-04-30 16:54:35 +0300

    created postsList and postsItem

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, {useState} from "react";
 import Counter from "./Components/Counter";
 import ClassCounter from "./Components/ClassCounter";
+import './Styles/App.css'
+import PostItem from "./Components/PostItem";
+import PostList from "./Components/PostList";
 
 function App() {
-    const [value, setValue] = useState('Text in input')
+    // const [value, setValue] = useState('Text in input')
+    const [posts, setPosts] = useState([
+        {id:1, title: 'Javascript', body: 'Description'},
+        {id:2, title: 'Html', body: 'Description'},
+        {id:3, title: 'React', body: 'Description'},]
+    )
+
 
   return (
-      <div className={App}>
-          <Counter/>
-          <ClassCounter/>
+      <div className='App'>
+          <form>
+              <input type="text" placeholder='Post name'/>
+              <input type="text" placeholder='Post description'/>
+          </form>
+        <PostList posts={posts} title='Posts list'/>
       </div>
   );
 }
