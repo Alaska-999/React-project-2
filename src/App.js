ef1edda287c311e9128bea7187adea4ec0968f3c
Alaska-999 <kristinagaranchuk@gmail.com>
2022-04-30 18:29:04 +0300

    created ui button with styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ClassCounter from "./Components/ClassCounter";
 import './Styles/App.css'
 import PostItem from "./Components/PostItem";
 import PostList from "./Components/PostList";
+import MyButton from "./Components/UI/button/MyButton";
 
 function App() {
     // const [value, setValue] = useState('Text in input')
@@ -19,6 +20,7 @@ function App() {
           <form>
               <input type="text" placeholder='Post name'/>
               <input type="text" placeholder='Post description'/>
+              <MyButton>Create post</MyButton>
           </form>
         <PostList posts={posts} title='Posts list'/>
       </div>
